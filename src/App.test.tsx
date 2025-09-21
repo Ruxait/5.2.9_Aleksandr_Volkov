158e75c116c0587d31c9ba475c34a6a3f5764f71
Aleksandr Volkov <aev7823@gmail.com>
2025-09-21 23:28:35 +0700

    Initial commit

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-import '@testing-library/jest-dom'
-
-test('renders hello message', () => {
-  render(<App />)
-  expect(screen.getByText(/hello/gi)).toBeInTheDocument()
-})
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import { Header } from './modules/Header'
+import { CartProvider } from './hooks/useCart'
+import { Catalog } from './modules/Catalog'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <CartProvider>
+        <Header />
+        <Catalog />
+      </CartProvider>
     </>
   )
 }
